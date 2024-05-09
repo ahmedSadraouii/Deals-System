04cf453c047fc9bde8971e2fae80e010f75b59ff
mohamedelleuch777 <usher7med@gmail.com>
2024-05-09 16:05:14 +0100

    added expired prop to CartItems

--- apps/customerweb/src/components/cart/cart-items.tsx
+++ apps/customerweb/src/components/cart/cart-items.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image';
 import { CartCounter } from './cart-counter';
 import { Card, CardHeader, CardBody, Divider } from '@nextui-org/react';
+import { NotAvailableCartItem } from './not-available-deal'; 
 
-export function CartItems() {
   // Fake array of items
   const items = [
     {
@@ -13,6 +13,7 @@ export function CartItems() {
       imageAlt: 'Description of your image',
       originalPrice: '1200,00€',
       discountedPrice: '900,00€',
+      available:true
     },
 
     {
@@ -23,24 +24,30 @@ export function CartItems() {
       imageAlt: 'Description of your image',
       originalPrice: '1200,00€',
       discountedPrice: '900,00€',
+      available:false
     },
   ];
-
+  interface CartItemsProps {
+    expired: boolean;
+  }
+  
+  export function CartItems({ expired }: CartItemsProps) {
   return (
     <Card className="bg-gray-100">
-      <CardHeader>
+      <CardHeader className='border-b pb-4'>
         <h1 className="pl-5 text-2xl font-bold leading-10">Dein Warenkorb</h1>
       </CardHeader>
-      <Divider />
-      <CardBody>
+      <CardBody className={`${expired ? 'opacity-10' : ''}`}>
         {items.map((item, index) => (
           <div
             className={`jus mb-10 flex items-center justify-between gap-20 px-5 ${
               index !== items.length - 1 ? 'border-b pb-4' : ''
-            }`}
+            } `}
             key={item.id}
           >
-            <div className="flex gap-4">
+            <div
+              className={`flex gap-4 ${!item.available ? 'opacity-10' : ''}`}
+            >
               <div className="hidden md:block">
                 <Image
                   src={item.imageSrc}
@@ -55,22 +62,29 @@ export function CartItems() {
                 <p className="underline">Entfernen</p>
               </div>
             </div>
-            <div className="flex flex-col items-center gap-10 md:flex-row">
-              <div>
-                <p className="flex justify-end line-through">
-                  {item.originalPrice}
-                </p>
-                <h1 className="text-2xl font-bold text-orange-600">
-                  {item.discountedPrice}
-                </h1>
+            {item.available ? (
+              <div className="flex flex-col items-center gap-10 md:flex-row">
+                <div>
+                  <p className="flex justify-end line-through">
+                    {item.originalPrice}
+                  </p>
+                  <h1 className="text-2xl font-bold text-orange-600">
+                    {item.discountedPrice}
+                  </h1>
+                </div>
+                <div>
+                  <CartCounter />
+                </div>
               </div>
-              <div>
-                <CartCounter />
+            ) : (
+              <div className="max-w-full lg:max-w-[30%]">
+                <NotAvailableCartItem />
               </div>
-            </div>
+            )}
           </div>
         ))}
       </CardBody>
+   
     </Card>
   );
 }
