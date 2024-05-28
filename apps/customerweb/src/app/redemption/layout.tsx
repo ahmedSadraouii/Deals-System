1f54194092bd7feaca485225e6cf8a0f1a3431a6
Mohamed Elleuch <mohamed.elleuch@amplicade.com>
2024-05-28 21:36:59 +0100

    new : creating redemption layout

---
+++ apps/customerweb/src/app/redemption/layout.tsx
@@ -0,0 +1,16 @@
+import type { ReactNode } from 'react';
+import { RedemptionStepper } from '@/components/redemption/redemption-steps';
+
+export interface CartLayoutProps {
+  children: ReactNode;
+}
+export default function Layout({ children }: CartLayoutProps) {
+  return (
+    <div className="container mx-auto flex flex-col items-center justify-center gap-8 px-4 py-14 md:px-0">
+      <div className="w-full lg:w-[70%] xl:w-[50%]">
+        <RedemptionStepper />
+      </div>
+      {children}
+    </div>
+  );
+}
