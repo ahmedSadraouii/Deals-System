54daead291fdb3cb9ebd5a63a79025fdd5c1a56b
Mohamed Elleuch <mohamed.elleuch@amplicade.com>
2024-05-28 21:41:33 +0100

    new: adding title and description on the main page of redemption

--- apps/customerweb/src/app/redemption/page.tsx
+++ apps/customerweb/src/app/redemption/page.tsx
@@ -1,7 +1,12 @@
 export default function Page() {
   return (
     <div>
-      <h1>Page</h1>
+      <h1 className="text-center text-4xl font-bold text-secondary">
+        Kassenbon-PIN eingeben
+      </h1>
+      <p className="text-aldi-blue opacity-50">
+        Gib folgend die 16-stellige PIN auf deinem Kassenbon ein.
+      </p>
     </div>
   );
 }
