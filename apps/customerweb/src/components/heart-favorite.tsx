040e43c5648485a449858f86883615cfc43c0070
Mohamed Elleuch <mohamed.elleuch@amplicade.com>
2024-06-11 14:42:31 +0100

    making the add and delete api calls inside the heart component

--- apps/customerweb/src/components/heart-favorite.tsx
+++ apps/customerweb/src/components/heart-favorite.tsx
@@ -1,10 +1,13 @@
-// src/components/HeartFavorite.tsx
-import React, { useCallback, useContext, useMemo } from 'react';
+'use client';
+
+import React, { useContext } from 'react';
+import { useSession } from 'next-auth/react';
 import { IconHeart } from './svg/icon-heart';
 import {
   FavoriteContext,
   FavoriteContextActionKind,
 } from '@/app/contexts/favorite/favorite-context';
+import { getFavoritesApiClient } from '@/utils/deals-api-client';
 
 interface HeartFavoriteProps {
   dealId: string;
@@ -12,24 +15,42 @@ interface HeartFavoriteProps {
 
 export function HeartFavorite({ dealId }: HeartFavoriteProps) {
   const favoriteContext = useContext(FavoriteContext);
+  const { data: session } = useSession();
+  const favoritesApi = getFavoritesApiClient({
+    accessToken: session?.accessToken,
+  });
+  async function addFavorite(dealId: string) {
+    await favoritesApi.addUserFavorite({
+      addFavoriteInputModel: { dealId },
+    });
+  }
+  async function removeFavorite(dealId: string) {
+    await favoritesApi.deleteUserFavorite({ dealId });
+  }
 
-  const toggleFavorite = useCallback(
-    (dealId: string) => {
-      const isDealFavored = favoriteContext.favoredDealIds.includes(dealId);
-      favoriteContext.dispatch({
-        type: isDealFavored
-          ? FavoriteContextActionKind.RemoveFavorite
-          : FavoriteContextActionKind.AddFavorite,
-        dealId,
-      });
-    },
-    [favoriteContext],
-  );
+  const toggleFavorite = async (dealId: string) => {
+    const isDealFavored = favoriteContext.favoredDealIds.includes(dealId);
 
-  const isFavored = useMemo(
-    () => favoriteContext.favoredDealIds.includes(dealId),
-    [dealId, favoriteContext.favoredDealIds],
-  );
+    try {
+      if (isDealFavored) {
+        await removeFavorite(dealId);
+        favoriteContext.dispatch({
+          type: FavoriteContextActionKind.RemoveFavorite,
+          dealId,
+        });
+      } else {
+        await addFavorite(dealId);
+        favoriteContext.dispatch({
+          type: FavoriteContextActionKind.AddFavorite,
+          dealId,
+        });
+      }
+    } catch (error) {
+      console.error('Error updating favorite status:', error);
+    }
+  };
+
+  const isFavored = favoriteContext.favoredDealIds.includes(dealId);
 
   return (
     <span
