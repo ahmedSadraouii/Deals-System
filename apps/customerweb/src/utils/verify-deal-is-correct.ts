623a490429c84cfd47b4e5c3f982d62f97bfe2b9
Felix Faust <felix.faust@amplicade.com>
2024-06-17 21:00:27 +0200

    remove dealId from deal properties in validation

--- apps/customerweb/src/utils/verify-deal-is-correct.ts
+++ apps/customerweb/src/utils/verify-deal-is-correct.ts
@@ -7,7 +7,6 @@ export function verifyDealIsCorrect(deal: UmbracoDeal): boolean {
     deal.name,
     deal.createDate,
     deal.route.path,
-    deal.properties?.dealId,
     deal.properties?.price,
     deal.properties?.supplier,
     deal.properties?.supplier?.id,
