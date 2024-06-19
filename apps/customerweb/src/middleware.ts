c8f8570a7245d62af49f693cee9e80ed0682c96e
aruss <arusslabs@gmail.com>
2024-06-20 00:55:39 +0200

    added basic auth middleware

---
+++ apps/customerweb/src/middleware.ts
@@ -0,0 +1,38 @@
+// middleware.ts
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+const USERNAME = 'demo';
+const PASSWORD = 'DealsDemo420!';
+
+function checkBasicAuth(req: NextRequest) {
+  const authHeader = req.headers.get('authorization');
+  if (!authHeader) {
+    return false;
+  }
+
+  const auth = authHeader.split(' ')[1];
+  const [username, password] = Buffer.from(auth, 'base64')
+    .toString()
+    .split(':');
+
+  return username === USERNAME && password === PASSWORD;
+}
+
+export const config = {
+  matcher: '/((?!api|_next|static|public|favicon.ico).*)',
+};
+
+export function middleware(request: NextRequest) {
+  if (process.env.NODE_ENV !== 'production') {
+    return NextResponse.next();
+  }
+
+  if (!checkBasicAuth(request)) {
+    const response = new NextResponse('Unauthorized', { status: 401 });
+    response.headers.set('WWW-Authenticate', 'Basic realm="Secure Area"');
+    return response;
+  }
+
+  return NextResponse.next();
+}
