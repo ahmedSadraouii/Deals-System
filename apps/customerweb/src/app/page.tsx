b45163a5f9dda75a6704381b9819093c81ffb045
aruss <arusslabs@gmail.com>
2024-06-25 00:27:25 +0200

    testing theory, startup time is too high

--- apps/customerweb/src/app/page.tsx
+++ apps/customerweb/src/app/page.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
-import { UmbracoContentPage } from '@/components/umbraco-cms/umbraco-content-page';
+
+// import { UmbracoContentPage } from '@/components/umbraco-cms/umbraco-content-page';
 
 export default function Page() {
-  return <UmbracoContentPage path="landing-page" />;
+  // return <UmbracoContentPage path="landing-page" />;
+
+  return <div>Hello there...</div>;
 }
