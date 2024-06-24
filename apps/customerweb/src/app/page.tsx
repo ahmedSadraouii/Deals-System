8fa77d4e258030c181f254f06c29174a14a38963
aruss <arusslabs@gmail.com>
2024-06-25 00:45:48 +0200

    ...

--- apps/customerweb/src/app/page.tsx
+++ apps/customerweb/src/app/page.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
-
-// import { UmbracoContentPage } from '@/components/umbraco-cms/umbraco-content-page';
+import { UmbracoContentPage } from '@/components/umbraco-cms/umbraco-content-page';
 
 export default function Page() {
-  // return <UmbracoContentPage path="landing-page" />;
-
-  return <div>Hello there...</div>;
+  return <UmbracoContentPage path="landing-page" />;
 }
