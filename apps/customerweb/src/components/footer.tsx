da793d35a31bc95223d62dc4c9d7504de608765d
Mohamed Elleuch <mohamed.elleuch@amplicade.com>
2024-07-09 14:36:27 +0100

    update footer design

--- apps/customerweb/src/components/footer.tsx
+++ apps/customerweb/src/components/footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import Link from 'next/link';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/utils/auth';
@@ -47,93 +48,148 @@ export async function Footer() {
         </div>
       </div>
       <footer className="bg-secondary text-white">
-        <div className="container mx-auto w-full p-4 py-16">
-          <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
-            <div>
-              <div className="mb-6 md:mb-0">
-                <a href="/" className="flex items-center">
-                  <img
-                    src="/logo-white.svg"
-                    className="me-3 h-24"
+        <div className="mx-auto w-full py-16 md:container md:p-4">
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+            <div className="flex flex-col items-center justify-center">
+              <div className="hidden md:block">
+                <Link href="/">
+                  <Image
+                    src="/logos/aldi-logo.svg"
+                    height={100}
+                    width={100}
+                    className="me-3 h-[10rem]"
                     alt="FlowBite Logo"
                   />
-                </a>
+                </Link>
+              </div>
+              <div className="mb-6 md:mb-0">
+                <Link href="/">
+                  <Image
+                    src="/logos/aldi-fire.svg"
+                    height={100}
+                    width={100}
+                    className="h-[10rem] w-48"
+                    alt="FlowBite Logo"
+                  />
+                </Link>
               </div>
             </div>
-            <div className="grid grid-cols-2">
-              <div>
-                <h2 className="mb-6 text-xl font-bold">Navigation</h2>
-                <ul>
-                  <li className="mb-4 text-neutral-50">
-                    <a href="/" className="hover:underline">
-                      Start
-                    </a>
-                  </li>
-                  <li className="mb-4 text-neutral-50">
-                    <a href="/cart" className="hover:underline">
-                      Warenkorb
-                    </a>
-                  </li>
-                  {/*<li className="mb-4">
-                    <a href="#" className="hover:underline">
-                      Kategorien
-                    </a>
-                  </li>
-                  <li className="mb-4">
-                    <a href="#" className="hover:underline">
-                      Aktuelles
-                    </a>
-                  </li>
-                  <li className="mb-4">
-                    <a href="#" className="hover:underline">
-                      Hilfe & Support
-                    </a>
-                  </li>*/}
-                </ul>
+            <div className="grid w-full grid-cols-1 md:grid-cols-2">
+              <div className="flex flex-col items-center justify-center border-b border-slate-100 pb-4 md:border-b-0	">
+                <div>
+                  <h2 className="mb-4 text-3xl font-[450]">Navigation</h2>
+                  <ul>
+                    <li className="mb-4 text-neutral-50">
+                      <Link href="/" className="hover:underline">
+                        Start
+                      </Link>
+                    </li>
+                    <li className="mb-4 text-neutral-50">
+                      <Link href="/auth" className="hover:underline">
+                        Anmelden
+                      </Link>
+                    </li>
+                    <li className="mb-4">
+                      <Link href="/auth" className="hover:underline">
+                        Registrieren
+                      </Link>
+                    </li>
+                    <li className="mb-4">
+                      <Link href="#" className="hover:underline">
+                        FAQ
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
               </div>
-              <div>
-                <ul className="mt-12">
-                  {session ? (
-                    <>
-                      <li className="mb-4">
-                        <a href="/profile/general" className="hover:underline">
-                          Profil
-                        </a>
-                      </li>
-                      <li className="mb-4">
-                        <a href="/profile/deals" className="hover:underline">
-                          Meine Deals
-                        </a>
-                      </li>
-                      <li className="mb-4">
-                        <a
-                          href="/profile/favorites"
-                          className="hover:underline"
-                        >
-                          Meine Favoriten
-                        </a>
-                      </li>
-                    </>
-                  ) : (
-                    <>
-                      <li className="mb-4">
-                        <a href="#" className="hover:underline">
-                          Anmelden
-                        </a>
-                      </li>
-                      <li className="mb-4">
-                        <a href="#" className="hover:underline">
-                          Registrieren
-                        </a>
-                      </li>
-                    </>
-                  )}
-                </ul>
+              <div className="flex flex-col items-center justify-center border-b border-slate-100 pb-4 md:border-b-0 ">
+                <div className="mr-[57px] md:mr-0">
+                  <ul className="mt-12">
+                    {session ? (
+                      <>
+                        <li className="mb-4">
+                          <Link
+                            href="/profile/general"
+                            className="hover:underline"
+                          >
+                            Warenkorb
+                          </Link>
+                        </li>
+                        <li className="mb-4">
+                          <Link
+                            href="/profile/general"
+                            className="hover:underline"
+                          >
+                            Profil
+                          </Link>
+                        </li>
+                        <li className="mb-4">
+                          <Link
+                            href="/profile/deals"
+                            className="hover:underline"
+                          >
+                            Meine Deals
+                          </Link>
+                        </li>
+                        <li className="mb-4">
+                          <Link
+                            href="/profile/favorites"
+                            className="hover:underline"
+                          >
+                            Merkliste
+                          </Link>
+                        </li>
+                      </>
+                    ) : (
+                      <>
+                        <li className="mb-4">
+                          <Link href="/auth" className="hover:underline">
+                            Anmelden
+                          </Link>
+                        </li>
+                        <li className="mb-4">
+                          <Link href="/auth" className="hover:underline">
+                            Registrieren
+                          </Link>
+                        </li>
+                      </>
+                    )}
+                  </ul>
+                </div>
               </div>
+            </div>
+            <div className="flex flex-col items-center justify-center border-b border-slate-100 pb-4 pt-4 md:border-b-0">
+              <ul className="flex h-full flex-col gap-4 md:mx-0 md:flex-row md:items-end">
+                <li>
+                  <Link href="/" className="text-nowrap hover:underline">
+                    Cookie-Einstellungen
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/" className="hover:underline">
+                    Datenschutz
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/" className="hover:underline">
+                    Impressum
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/" className="hover:underline">
+                    Nutzungsbedingungen
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/" className="hover:underline">
+                    Widerruf{' '}
+                  </Link>
+                </li>
+              </ul>
             </div>
           </div>
           <div className="mt-10 justify-center sm:flex sm:items-center">
-            <span className="text-sm text-neutral-600">
+            <span className="text-sm text-white">
               &copy; 2024{' '}
               <Link href="/" className="hover:underline">
                 Aldi-Deals&trade;
